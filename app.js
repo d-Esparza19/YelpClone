e618168e0da2d7464d31ee28f599edcd8de5db70
Daniel Esparza <15037512+d-Esparza19@users.noreply.github.com>
2020-09-18 14:29:08 -0500

    Responsive styling added.

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+var campgrounds = [
+    {name:'test', image:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Camping_in_Nova_Scotia_%28Hine%29.png/1920px-Camping_in_Nova_Scotia_%28Hine%29.png"}
+    ,{name:'test - 2', image:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/00_0968_Sweden%2C_place_Fj%C3%A4lkinge_%28Camping%29.jpg/1280px-00_0968_Sweden%2C_place_Fj%C3%A4lkinge_%28Camping%29.jpg"}
+    ,{name:'test - 3', image:"https://upload.wikimedia.org/wikipedia/commons/7/70/A_camping-place_in_the_wilderness_of_Sinai.jpg"}
+];
 
 port = 3000
 
+app.use(bodyParser.urlencoded({extended: true}));
 // set view engine to Embedded java script
 app.set("view engine","ejs");
 
@@ -13,13 +21,21 @@ app.get('/', (req, res) => {
 
 //
 app.get('/campgrounds',(req,res)=>{
-    var campgrounds = [
-        {name:'test', image:"https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Camping_in_Nova_Scotia_%28Hine%29.png/1920px-Camping_in_Nova_Scotia_%28Hine%29.png"}
-        ,{name:'test - 2', image:"https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/00_0968_Sweden%2C_place_Fj%C3%A4lkinge_%28Camping%29.jpg/1280px-00_0968_Sweden%2C_place_Fj%C3%A4lkinge_%28Camping%29.jpg"}
-        ,{name:'test - 3', image:"https://upload.wikimedia.org/wikipedia/commons/7/70/A_camping-place_in_the_wilderness_of_Sinai.jpg"}
-    ]
-
     res.render("campgrounds",{campgrounds:campgrounds});
+});
+
+app.post('/campgrounds', function (req, res) {
+    //get data from form and add to campground array
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name:name, image:image}
+    campgrounds.push(newCampground);
+    //redirect to campgrounds
+    res.redirect('/campgrounds');
+  });
+
+app.get('/campgrounds/new', (req,res)=>{
+    res.render('new.ejs');
 });
 
 
